17839a31dca701258bd677e04f3c2faf5630a39a
Isaac <izlimhy@gmail.com>
2023-05-26 16:43:18 +0800

    changed name to SIlverLink

--- orbital-app/App.js
+++ orbital-app/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>fuck you</Text>
+      <Text>SilverLink</Text>
       <StatusBar style="auto" />
     </View>
   );
