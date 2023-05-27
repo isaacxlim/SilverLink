c11de76416aa1f49ee5b8a02c29e9c4f419481ce
Isaac <izlimhy@gmail.com>
2023-05-27 16:07:42 +0800

    changes to app.js and login.js

--- orbital-app/screens/Login.js
+++ orbital-app/screens/Login.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 
-import {
-    StyledContainer,
-    InnerContainer,
-    PageLogo,
-    PageTitle
-} from './../components/styles'; 
+//formik
+import { Formik } from 'expo-status-bar';
+//icons
+import { Octicons } from '@expo/vector-icons';
+
+import { StyledContainer, InnerContainer, PageLogo, PageTitle, SubTitle, StyledFormArea, LeftIcon, StyledInputLabel, StyledTextInput, RightIcon, Colors} from './../components/styles'; 
+import { View } from 'react-native';
+
+//Colors
+const {brand} = Colors;
 
 const Login = () => {
     return (
@@ -15,9 +19,29 @@ const Login = () => {
             <InnerContainer>
                 <PageLogo resizeMode="cover" source={require('./../assets/SilverLinkLogo.png')} />
                 <PageTitle>SilverLink</PageTitle>
+                <SubTitle> Account Login</SubTitle>
+
+                <Formik
+                    initialValues={{email: '', password: ''}}
+                    onSubmit={(values) => {
+                        console.log(values);
+                    }}
+                > 
+                {({handleChange, HandleBlur, HandleSubmit, values}) => (<StyledFormArea></StyledFormArea>)}
+                </Formik>
             </InnerContainer>
         </StyledContainer>
     );
+};
+
+const MyTextInput = ({label, icon, ...props}) => {
+    return (<View> 
+        <LeftIcon>
+            <Octicons name={icon} size ={30} color={brand}/>
+        </LeftIcon>
+        <StyledInputLabel>{label}</StyledInputLabel>
+        <StyledTextInput {...props}/>
+    </View>)
 }
 
 export default Login;
