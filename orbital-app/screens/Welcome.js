3326f34bede3a0d14c8b78c23d51b388ea5d324b
Isaac <izlimhy@gmail.com>
2023-05-27 18:00:21 +0800

    welcome slight updates

---
+++ orbital-app/screens/Welcome.js
@@ -0,0 +1,117 @@
+import React, {useState} from 'react';
+import { StatusBar } from 'expo-status-bar';
+
+//formik
+import { Formik } from 'formik';
+//icons
+import { Octicons, Ionicons} from '@expo/vector-icons';
+//Colors
+
+import { StyledContainer, 
+    InnerContainer, 
+    PageLogo, 
+    PageTitle, 
+    SubTitle, 
+    StyledFormArea, 
+    LeftIcon, 
+    StyledInputLabel, 
+    StyledTextInput, 
+    RightIcon, 
+    StyledButton, 
+    ButtonText, 
+    Colors,
+    MsgBox, 
+    Line,
+    ExtraView,
+    ExtraText,
+    TextLink,
+    TextLinkContent
+} from './../components/styles'; 
+
+import { View } from 'react-native';
+
+//Colors
+const {brand, darkLight} = Colors;
+
+const Welcome = () => {
+    const [hidePassword, setHidePassword] = useState(true);
+
+
+    return (
+        <StyledContainer>
+            <StatusBar style = "dark" />
+            <InnerContainer>
+                <PageLogo resizeMode="cover" source={require('./../assets/SilverLinkLogo.png')} />
+                <PageTitle>SilverLink</PageTitle>
+                <SubTitle> Account Login</SubTitle>
+
+                <Formik
+                    initialValues={{email: '', password: ''}}
+                    onSubmit={(values) => {
+                        console.log(values);
+                    }}
+                > 
+                    {({handleChange, handleBlur, handleSubmit, values}) => (
+                        <StyledFormArea>
+                            <MyTextInput 
+                                label="Email Address"
+                                icon="mail"
+                                placeholder="name@example.com"
+                                placeholderTextColor={darkLight}
+                                onChangeText={handleChange('email')}
+                                onBlur={handleBlur('email')}
+                                value={values.email}
+                                keyboardType="email-address"
+                            />
+
+                            <MyTextInput 
+                                label="Password"
+                                icon="lock"
+                                placeholder="* * * * * * * *"
+                                placeholderTextColor={darkLight}
+                                onChangeText={handleChange('password')}
+                                onBlur={handleBlur('password')}
+                                value={values.password}
+                                secureTextEntry={hidePassword}
+                                isPassword={true}
+                                hidePassword={hidePassword}
+                                setHidePassword={setHidePassword}
+                            />
+                            <MsgBox>...</MsgBox>
+                            <StyledButton onPress={handleSubmit}>
+                                <ButtonText>
+                                    Login
+                                </ButtonText>
+                            </StyledButton>
+                            <Line />
+                            <ExtraView>
+                                <ExtraText>Don't have an account already? </ExtraText>
+                                <TextLink>
+                                    <TextLinkContent>Signup</TextLinkContent>
+                                </TextLink>
+
+                            </ExtraView>
+
+                        </StyledFormArea>)}
+                    </Formik>
+            </InnerContainer>
+        </StyledContainer>
+    );
+};
+
+const MyTextInput = ({ label, icon, isPassword, hidePassword, setHidePassword, ...props }) => {
+    return (<View> 
+        <LeftIcon>
+            <Octicons name={icon} size ={30} color={brand}/>
+        </LeftIcon>
+        <StyledInputLabel>{label}</StyledInputLabel>
+        <StyledTextInput {...props} />
+        {isPassword && (
+            <RightIcon onPress={() => setHidePassword(!hidePassword)}>
+                <Ionicons name={hidePassword ? 'md-eye-off' : 'md-eye'} size={30} color={darkLight}/>
+            </RightIcon>
+        )}
+    </View>);
+};
+
+export default Welcome;
