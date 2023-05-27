1b36edad704197c0b7632d9d5428eeab83fbceb8
Phelan <phelanlee2001@gmail.com>
2023-05-27 14:49:39 +0800

    adding components (styles)

---
+++ orbital-app/components/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+//colors
+export const Colors = {
+    primary: "#ffffff",
+    secondary: "#E5E7EB",
+    tertiary: "#1F2937",
+    darkLight: "#9CA3AF",
+    brand: "#6D28D9",
+    green: "#10B981",
+    red: "#EF4444",
+};
